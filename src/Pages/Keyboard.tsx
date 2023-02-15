21f6f16b08055979bf3eeeb316e067903d413234
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2023-02-15 18:08:03 -0300

    Alpha 1.0.3

--- src/Pages/Keyboard.tsx
+++ src/Pages/Keyboard.tsx
@@ -6,8 +6,12 @@ import { Letters } from '../components/Letters'
 import { Words } from '../components/Words'
 export default function Keyboard() {
   const [phrase, setPhrase] = useState<string[]>([])
+  const [isKeyboardAllowed, setIsKeyboardAllowed] = useState(false)
 
   function addWord(word: string) {
+    if (!isKeyboardAllowed) {
+      return
+    }
     setPhrase((state) => [...state, word])
   }
 
@@ -15,16 +19,28 @@ export default function Keyboard() {
     if (phrase.length === 0) {
       return
     }
+    if (!isKeyboardAllowed) {
+      return
+    }
+
     const newPhrase = [...phrase]
     newPhrase.pop()
 
     setPhrase(newPhrase)
   }
 
+  function startOrStop() {
+    setIsKeyboardAllowed(!isKeyboardAllowed)
+  }
+
   return (
     <main>
-      <div className="text-7xl max-lg:text-4xl">
-        <Input phrase={phrase} />
+      <div className="text-7xl max-lg:text-4xl transition-all">
+        <Input
+          phrase={phrase}
+          isKeyboardAllowed={isKeyboardAllowed}
+          startOrStop={startOrStop}
+        />
         <Words onAddWord={addWord} />
         <Letters onAddWord={addWord} onDeleteLastWord={deleteLastWord} />
       </div>
