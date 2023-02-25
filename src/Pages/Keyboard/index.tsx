e74d1127222c3991a55cf9e2a55278d0d26296eb
Gabriel Sousa <79923219+Gabriel-Sousa@users.noreply.github.com>
2023-02-25 13:30:23 -0300

    Voz do google

--- src/Pages/Keyboard/index.tsx
+++ src/Pages/Keyboard/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { version } from '../../../package.json'
 import { Input } from './components/Input'
@@ -13,10 +13,64 @@ export default function Keyboard({ time }: KeyboardProps) {
   const [phrase, setPhrase] = useState<string[]>([])
   const [isKeyboardAllowed, setIsKeyboardAllowed] = useState(false)
 
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then(function (stream) {
+      // console.log('Permissão para o áudio concedida.')
+    })
+    .catch(function () {
+      // console.log('Erro ao solicitar permissão para o áudio: ' + err)
+    })
+
+  if ('speechSynthesis' in window) {
+    console.log('Web Speech API é compatível com este navegador')
+  } else {
+    alert('Web Speech API não é compatível com este navegador')
+  }
+
+  useEffect(() => {
+    if (!isKeyboardAllowed && phrase.length > 0) {
+      const word = phrase
+      console.log(word)
+
+      const stringWords = phrase.join('')
+      const arrayWords = stringWords.split(' ')
+      const stringMsg = arrayWords.join('')
+
+      const speech = new SpeechSynthesisUtterance(stringMsg)
+      const voices = speechSynthesis.getVoices()
+      speech.voice = voices[1]
+      speech.volume = 2
+      speech.rate = 1
+      speech.pitch = 1
+
+      window.speechSynthesis.speak(speech)
+    }
+  }, [phrase, isKeyboardAllowed])
+
+  useEffect(() => {
+    if (phrase.length === 0) {
+      return
+    }
+
+    const word = phrase[phrase.length - 1]
+
+    const speech = new SpeechSynthesisUtterance(word)
+    const voices = speechSynthesis.getVoices()
+    speech.text = word
+    speech.voice = voices[1]
+    speech.volume = 2
+    speech.rate = 1
+    speech.pitch = 1
+
+    window.speechSynthesis.speak(speech)
+  }, [phrase])
+
   function addWord(word: string) {
     if (!isKeyboardAllowed) {
       return
     }
+
     setPhrase((state) => [...state, word])
   }
 
